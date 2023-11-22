a59a0a477194c01e0aefeca0b28aa175a47890bb
hassanrnt <hassanritouny97@gmail.com>
2023-11-22 11:24:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import NavBar from './navbar'
+import Card from './Card'
+
+function App() {
+  return (
+ 
+    <div>
+      <NavBar />
+<div className="container">
+  <Card />
+  <Card />
+  <Card />
+</div>
+      
+    </div>
+   
+  )
+}
+export default App;
+
